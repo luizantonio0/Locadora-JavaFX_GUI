ee9f3d8207d7a761e71346a878b2d4fb387f7254
notfakeluiz <antonio.luiz@ufpr.br>
2025-05-01 16:19:40 -0300

    init commit

--- src/main/java/com/locadorafx/HelloController.java
+++ src/main/java/com/locadorafx/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome tdo JavaFX Application!");
+        welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
