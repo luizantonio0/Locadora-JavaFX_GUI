808f53f68495b6c78a6d8058a26deb4db7b22d70
Luiz Antônio Gonçalves <antonio.luiz@ufpr.br>
2025-05-16 19:43:30 -0300

    update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,9 +3,6 @@ module com.locadorafx {
         requires javafx.fxml;
         //requires jasypt;
 
-        opens com.locadorafx.Entities.Locacao to javafx.base;
-        opens com.locadorafx.Entities.Veiculos to javafx.base;
-        opens com.locadorafx.Entities.Clientes to javafx.base;
         opens com.locadorafx.Controllers to javafx.fxml;
         opens com.locadorafx to javafx.fxml;
         exports com.locadorafx;
